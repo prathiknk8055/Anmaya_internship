3b2a095ac79f34acd1201665e1533576f27566d8
Prathik N K <prathiknk8055@gmail.com>
2025-06-19 10:59:01 +0530

    update

--- Assignment3/3b/testbench.sv
+++ Assignment3/3b/testbench.sv
@@ -1,12 +1,26 @@
-// Code 2x1 Mux with D FF delay
-module mux2x1withdelay(input clk,din,sel, output out);
-  reg q;
-  always @(posedge clk)
-    begin
-      q <= sel?din:q;
-    end
-  assign out = q;
+// Testbench for mux2x1 with D FF code
+
+module mux2x1withdelay_tb;
+  reg clk,din,sel;
+  wire out;
+  
+  mux2x1withdelay dut(clk,din,sel,out);
+  
+  always #5 clk = ~clk;
+  
+  initial begin
+    $monitor("Time = %0t, sel= %b,din=%b,out=%b",$time,sel,din,out);
+    $dumpfile("mux2x1withdelay.vcd");
+    $dumpvars(0,mux2x1withdelay_tb);
+    
+    clk=0;sel=0;din=0;#3;
+    sel=1;din=1;#10;
+    sel=0;din=1;#10;
+    sel=0;din=1;#10;
+    sel=1;din=0;#10;
+    sel=0;din=0;#10;
+    sel=0;din=0;#10;
+    
+    $finish;
+  end
 endmodule
-      
-
-
